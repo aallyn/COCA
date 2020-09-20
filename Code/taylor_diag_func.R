6f79ea4ca9ae69200263e3f84ed33a6077df0c2b
Andrew Allyn <andrew.allyn@gmail.com>
2020-09-20 13:00:50 -0400

    Taylor Diagram function update

--- Code/taylor_diag_func.R
+++ Code/taylor_diag_func.R
@@ -1,62 +1,104 @@
 #####
-## SDM prediction validation plots
+## Taylor Diagram Function
 #####
 
-## Going to try to replicate the Taylor Diagram, which combines information on the root mean square error (standard deviation of the residuals -- how far off our predictions are on the measurement scale), coefficient of determination (correlation between predicted and observed value, as well as deviation off of the 1:1 line as you could have a correlation of 1 and be systematically biased away from 1:1 line), and bias (ratio of standard deviation in the model to the data to assess if the model is under or over predicting the amount of variability observed in the real world, making things too smooth or too dynamic).
+# Helper functions: correlation coefficient and bias ----------------------
+corrcoeff_func_simp<- function(df){
+  df.use<- df %>%
+    drop_na(obs, mod)
+  mean.obs<- mean(df.use$obs)
+  mean.mod<- mean(df.use$mod)
+  sd.obs<- sd(df.use$obs)
+  sd.mod<- sd(df.use$mod)
+  samps<- nrow(df.use)
+  
+  out<- ((1/samps)*(sum((df.use$mod - mean.mod)*(df.use$obs - mean.obs))))/(sd.obs*sd.mod)
+  return(out)
+}
 
-library(plotrix) # Includes a taylor.diagram function, with arguments for reference and model values. Going to modify this a bit I think...
-library(tidyverse)
+bias_func_simp<- function(df){
+  df.use<- df %>%
+    drop_na(obs, mod)
+  out<- sd(df.use$mod)/sd(df.use$obs)
+  return(out)
+}
 
-# Model results
-mod.stats<- read_csv("~/Github/COCA/Results/NormalVoting_BiomassIncPresNoExposure_03152019/mod.results.csv")
 
-taylor_diagram_func<- function(mydata, obs = "obs", mod = "mod", group = NULL, type = "default", 
- normalise = FALSE, cols = "brewer1", rms.col = "darkgoldenrod", cor.col = "black", arrow.lwd = 3, annotate = "centred\nRMS error", key = TRUE, key.title = group, key.columns = 1, key.pos = "right", strip = TRUE, auto.text = TRUE, ...) {
+# Main Taylor Diagram function --------------------------------------------
+taylor_diagram_func<- function(dat, obs = "obs", mod = "mod", group = NULL, out.dir, grad.corr.lines = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1), pcex = 1, cex.axis = 1, normalize = TRUE, mar = c(5, 4, 6, 6), sd.r = 1, pt.col = NULL, pt.cols = NULL, example = FALSE) {
   
   ## Details
-  # This function plots a Taylor Diagram of model prediction accuracy, sumamrizing the root mean square error, the coefficient of determination, and the ration of standard deviations. 
+  # This function plots a Taylor Diagram of model prediction accuracy, sumamrizing the root mean square error, the coefficient of determination, and the ratio of standard deviations. 
   
   # Args:
-  # mod.stats = Path to the model results csv folder, which needs at least the three values (RMSE, Coeff Determination, SD ratio)
-  # group = Grouping variable -- defaults to species
-  # out.path = Path to save the Taylor Diagram
-  
+  # dat = data frame with observations and model predictions, as well as group if necessary
+  # obs = Column name for the observation response
+  # mod = Column name for the modeled response
+  # group = Grouping variable, used for comparing different species/ages or stages/models, etc
+  # out.dir = Directory where to save the Taylor Diagram plot
+  # ... All these other things correspond to some of the aesthetics of the plot. pt.col gives color if just plotting one point (group, model), pt.cols is a vector of colors for plotting multiple points (groups, models) on one plot.
+
   # Returns: NULL; saves plot to output directory
   
   ## Start function
   # Install libraries
-  library_check(c("tidyverse"))
+  library(tidyverse)
   
   # Set arguments for debugging -- this will NOT run when you call the function. Though, you can run each line inside the {} and then you will have everything you need to walk through the rest of the function.
-  if(FALSE){
-    dat<- "~/Github/COCA/Results/NormalVoting_BiomassIncPresNoExposure_03152019/mod.results.csv"
-    group<- "COMNAME"
-    out.path<- "~/Desktop/"
-  }
-
-  if(FALSE){
-    mod.res
+  if(example){
+    # Create a data set with observations and predictions
+    data(trees)
+    tree.mod<- lm(Volume ~ Girth, data = trees)
+    trees$Volume.pred<- as.numeric(predict(tree.mod))
+    dat<- trees
+    obs<- "Volume"
+    mod<- "Volume.pred"
+    group<- NULL
+    out.dir<- "~/Desktop/"
     grad.corr.lines = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1)
     pcex = 1
     cex.axis = 1
     normalize = TRUE
-    mar = c(5, 4, 6, 6)  
+    mar = c(5, 4, 6, 6)
+    sd.r = 1
+    pt.col<- "#006d2c"
   }
   
-  # Real data
-  mod.res<- read_csv(mod.stats) %>%
-    drop_na(RMSE.SDM.B, CorrCoeff.SDM.B, Bias.SDM.B, RMSE.NEVA.B, CorrCoeff.NEVA.B, Bias.NEVA.B) %>%
-    filter(., AUC.SDM > 0.7)
+  # Some house keeping -- rename the obs and mod columns to work with generic functions
+  old.names<- c(obs, mod)
+  new.names<- c("obs", "mod")
+  dat<- dat %>%
+    rename_at(vars(old.names), ~ new.names)
   
+  # Calculate the correlation coefficient and bias, two stats needed for Taylor Diagram. Flexibility to group and then calculate stats by group (e.g., species, model, etc).
+  if(is.null(group)){
+    mod.stats<- dat %>%
+      nest(data = everything()) %>%
+      mutate(., "CorrCoeff" = as.numeric(map(data, corrcoeff_func_simp)),
+             "Bias" = as.numeric(map(data, bias_func_simp)))
+  } else {
+    # Group by group and calculate stats
+    mod.stats<- dat %>%
+      group_by(group) %>%
+      nest() %>%
+      mutate(., "CorrCoeff" = as.numeric(map(data, corrcoeff_func_simp)),
+             "Bias" = as.numeric(map(data, bias_func_simp)))
+  }
+
+  # Now plot creation....
   # Getting maxSD for plotting
-  maxsd<- 1.5 * max(mod.res$Bias.SDM.B, 1)
+  maxsd<- max(mod.stats$Bias, 1)
   
   # Empty plot first
   # Creating empty plot first
   plot.base<- ggplot() + 
-    scale_x_continuous(name = "Standard deviation (normalized)", limits = c(0, maxsd), breaks = seq(from = 0, to = maxsd, by = 0.5), expand = c(0, 0, 0.05, 0)) +
-    scale_y_continuous(name = "Standard deviation (normalized)", limits = c(0, maxsd), breaks = seq(from = 0, to = maxsd, by = 0.5), expand = c(0, 0, 0.05, 0)) +
-    theme_classic()
+    scale_x_continuous(name = "Standard deviation (normalized)", limits = c(0, maxsd+0.01), breaks = seq(from = 0, to = maxsd, by = 0.5), expand = c(0, 0.01)) +
+    scale_y_continuous(name = "Standard deviation (normalized)", limits = c(-0.015, maxsd), breaks = seq(from = 0, to = maxsd, by = 0.5), expand = c(0, 0.01)) +
+    theme_bw() +
+    theme(axis.text=element_text(size=14),
+          axis.title=element_text(size=16, face="bold"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()) 
   
   # Coeff D rays 
   for(i in 1:length(grad.corr.lines)){
@@ -73,8 +115,8 @@ taylor_diagram_func<- function(mydata, obs = "obs", mod = "mod", group = NULL, t
   
   # Add rays
   plot.coeffd<- plot.base +
-    geom_line(data = coeffd.rays.df, aes(x = x, y = y, group = Ray), lty = "longdash")
-  
+    geom_line(data = coeffd.rays.df, aes(x = x, y = y, group = Ray), lty = "longdash", col = "lightgray") 
+    
   coeffd.labs<- coeffd.rays.df %>%
     group_by(Ray) %>%
     summarize(., 
@@ -85,27 +127,27 @@ taylor_diagram_func<- function(mydata, obs = "obs", mod = "mod", group = NULL, t
   coeffd.labs$Label<- grad.corr.lines
   
   plot.coeffd<- plot.coeffd +
-    geom_label(data = coeffd.labs, aes(x = x, y = y, label = Label), fill = "white", label.size = NA)
+    geom_label(data = coeffd.labs, aes(x = x, y = y, label = Label), fill = "white", label.size = NA, size = 5)
   
   # SD arcs
   # Need to add in SD arcs
-  sd.arcs<- seq(from = 0, to = maxsd, by = 1)
+  sd.arcs<- seq(from = 0, to = maxsd, by = 0.5)
   
   for(i in 1:length(sd.arcs)){
     x.vec<- sd.arcs[i]*cos(seq(0, pi/2, by = 0.03))
     y.vec<- sd.arcs[i]*sin(seq(0, pi/2, by = 0.03))
     
     if(i ==1){
-      sd.arcs.df<- data.frame("Arc" = rep(1, length(x.vec)), "x" = x.vec, "y" = y.vec)
+      sd.arcs.df<- data.frame("Arc" = rep(sd.arcs[1], length(x.vec)), "x" = x.vec, "y" = y.vec)
     } else {
-      temp<- data.frame("Arc" = rep(i, length(x.vec)), "x" = x.vec, "y" = y.vec)
+      temp<- data.frame("Arc" = rep(sd.arcs[i], length(x.vec)), "x" = x.vec, "y" = y.vec)
       sd.arcs.df<- bind_rows(sd.arcs.df, temp)
     }
   }
   
   # Add arcs to plot.base
   plot.sd<- plot.coeffd +
-    geom_line(data = sd.arcs.df, aes(x = x, y = y, group = Arc), lty = "dotted") 
+    geom_line(data = sd.arcs.df, aes(x = x, y = y, group = Arc), lty = "dotted", color = "lightgray") 
   
   # Now gamma? -- Standard deviation arcs around the reference point
   gamma<- pretty(c(0, maxsd), n = 4)[-1]
@@ -124,9 +166,9 @@ taylor_diagram_func<- function(mydata, obs = "obs", mod = "mod", group = NULL, t
     y.vec<- ycurve[startcurve:endcurve]
     
     if(gindex ==1){
-      gamma.df<- data.frame("Gamma" = rep(1, length(x.vec)), "x" = x.vec, "y" = y.vec)
+      gamma.df<- data.frame("Gamma" = rep(gamma[1], length(x.vec)), "x" = x.vec, "y" = y.vec)
     } else {
-      temp<- data.frame("Gamma" = rep(gindex, length(x.vec)), "x" = x.vec, "y" = y.vec)
+      temp<- data.frame("Gamma" = rep(gamma[gindex], length(x.vec)), "x" = x.vec, "y" = y.vec)
       gamma.df<- bind_rows(gamma.df, temp)
     }
   }
@@ -143,22 +185,41 @@ taylor_diagram_func<- function(mydata, obs = "obs", mod = "mod", group = NULL, t
     summarize("x" = mean(x, na.rm = TRUE), 
               "y" = median(y, na.rm = TRUE))
   
+  inflection_func<- function(df){
+    d1<- diff(df$y)/diff(df$x)    
+    pt.id<- which.max(d1)
+    pt.out<- df[pt.id,]
+    pt.out$y<- rep(0, nrow(pt.out))
+    return(pt.out)
+  }
+  
+  gamma.labs<- gamma.df %>%
+    group_by(Gamma) %>%
+    nest() %>%
+    summarize("pt" = map(data, inflection_func)) %>%
+    unnest(cols = c(pt))
+  
   plot.gamma<- plot.gamma +
     geom_label(data = gamma.labs, aes(x = x, y = y, label = Gamma), fill = "white", label.size = NA)
   
   # Add in reference point
   plot.all<- plot.gamma +
-    geom_point(aes(x = sd.r, y = 0), color = "red")
+    geom_point(aes(x = sd.r, y = 0), color = "black", size = 3.5) 
   
-  # Add in species points
-  mod.results.td<- mod.results %>%
-    filter(., CorrCoeff.SDM.B >= 0) %>%
-    mutate(., "TD.X" = Bias.SDM.B * CorrCoeff.SDM.B,
-           "TD.Y" = Bias.SDM.B * sin(acos(CorrCoeff.SDM.B)))
+  # Add in reference points
+  mod.td<- mod.stats %>%
+    mutate(., "TD.X" = Bias * CorrCoeff,
+           "TD.Y" = Bias * sin(acos(CorrCoeff)))
   
-  plot.all.f<- plot.all +
-    geom_point(data = subset(mod.results.td, SEASON == "FALL"), aes(x = TD.X, y = TD.Y, color = COMNAME), show.legend = FALSE)
-  
-  plot.all.s<- plot.all +
-    geom_point(data = subset(mod.results.td, SEASON == "SPRING"), aes(x = TD.X, y = TD.Y, color = COMNAME), show.legend = FALSE)
+  if(is.null(group)){
+    plot.td<- plot.all +
+      geom_point(data = mod.td, aes(x = TD.X, y = TD.Y), color = pt.col, size = 4) +
+      geom_text(aes(label = "Correlation coefficient", x = 0.8, y = 0.75), fontface = "bold", angle = -38, size = 5.25)
+  } else {
+    plot.td<- plot.all +
+      geom_point(data = mod.td, aes(x = TD.X, y = TD.Y, color = pt.cols), size = 3) +
+      geom_text(aes(label = "Correlation coefficient", x = 0.8, y = 0.75), fontface = "bold", angle = -38, size = 5.25)
+  }
+  ggsave(paste(out.dir, "_TaylorDiagram.jpg", sep = ""), plot.td)
 }
+
